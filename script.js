91b78956fa2bb86ee5f454bec51388c57968c021
alienfunbug <alienfunbug@gmail.com>
2024-04-23 17:11:08 -0700

    final changes until mobile

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-const myLibrary = [
+/*const myLibrary = [
   { title: "The Great Gatsby", author: "F. Scott Fitzgerald", pages: 180, haveRead: false },
   { title: "To Kill a Mockingbird", author: "Harper Lee", pages: 281, haveRead: false },
   { title: "1984", author: "George Orwell", pages: 328, haveRead: false }
-];
+]; */
 
+/*
+function addBookToLibrary(name,author,pages,haveRead) {
+    let newBook = new Book(name,author,pages,haveRead);
+    myLibrary.push(newBook);
+} */
+
+
+
+/*delete card when button is clicked */
 function removeElementAndChildren(element) {
   element.parentNode.removeChild(element);
 }
 
 let multiSelectButtons = document.querySelectorAll("button.card-close");
 
+/* add delete function to each delete button on cards*/
 multiSelectButtons.forEach(function (btnCard) {
   btnCard.addEventListener("click", function () {
     removeElementAndChildren(btnCard.parentNode);
@@ -94,35 +104,28 @@ function addBookToLibrary(title, author, pages, haveRead) {
 
 };
 
+
+/*book constructure */
 function Book(title, author, pages, haveRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.haveRead = false;
 }
-/*
-function addBookToLibrary(name,author,pages,haveRead) {
-    let newBook = new Book(name,author,pages,haveRead);
-    myLibrary.push(newBook);
-} */
 
 
-
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
-
+/*modal functionality */
 const openModal = () => {
   const modal = document.getElementById("modal");
   modal.showModal();
 };
-
+/*modal functionality */
 const closeModal = () => {
   const modal = document.getElementById("modal");
   modal.close();
 };
 
-
+/*take add book inputs from modal and send to create card */
 const bookForm = document.getElementById("book-form");
 
 document.getElementById("book-form").addEventListener("submit", function (event) {
@@ -132,9 +135,8 @@ document.getElementById("book-form").addEventListener("submit", function (event)
   const pages = document.getElementById("book-pages").value;
   const haveRead = document.getElementById("have-read").checked;
   bookForm.reset();
-  console.log(myLibrary);
   addBookToLibrary(title, author, pages, haveRead);
-  console.log(myLibrary);
+  console.log("Added " + title,author,pages,haveRead + " to Library");
 });
 
 
